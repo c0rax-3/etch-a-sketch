a886315878f8421961c16dddc25db522512e2d2d
c0rax-3 <221375761+c0rax-3@users.noreply.github.com>
2026-01-11 15:09:40 +0000

    Create div boxes on canvas

--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
 const canvas = document.querySelector('.canvas');
+const gridSize = 30;
 
-
-for (let i = 0; i < 1000; i++) {
-    const square = document.createElement('div');
+// for (let i = 0; i < 1000; i++) {
+//     const square = document.createElement('div');
     
 
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-    });
+//     square.addEventListener('mouseover', () => {
+//         square.style.backgroundColor = 'black';
+//     });
 
-    canvas.appendChild(square);
+//     canvas.appendChild(square);
+// }
+
+for (let i = 0; i < gridSize; i++) {
+    const columnDiv = document.createElement('div');
+    columnDiv.className = 'column';
+    for (let i = 0; i < gridSize; i++) {
+        const box = document.createElement('div');
+        columnDiv.appendChild(box)
+        box.className = 'box'
+    }
+
+    canvas.appendChild(columnDiv)    
 }
 
 const clearBtn = document.querySelector('.clear');
