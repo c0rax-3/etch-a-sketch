e5b101243c4ef561f1854a785b174b44e8d89647
c0rax-3 <221375761+c0rax-3@users.noreply.github.com>
2026-01-11 15:16:05 +0000

    Add drawing tools to color in boxes

--- sketch.js
+++ sketch.js
@@ -1,34 +1,25 @@
-const canvas = document.querySelector('.canvas');
+const canvas = document.querySelector(".canvas");
 const gridSize = 30;
 
-// for (let i = 0; i < 1000; i++) {
-//     const square = document.createElement('div');
-    
-
-//     square.addEventListener('mouseover', () => {
-//         square.style.backgroundColor = 'black';
-//     });
-
-//     canvas.appendChild(square);
-// }
-
 for (let i = 0; i < gridSize; i++) {
-    const columnDiv = document.createElement('div');
-    columnDiv.className = 'column';
-    for (let i = 0; i < gridSize; i++) {
-        const box = document.createElement('div');
-        columnDiv.appendChild(box)
-        box.className = 'box'
-    }
-
-    canvas.appendChild(columnDiv)    
+  const columnDiv = document.createElement("div");
+  columnDiv.className = "column";
+  for (let i = 0; i < gridSize; i++) {
+    const box = document.createElement("div");
+    box.className = "box";
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = "black";
+    });
+    columnDiv.appendChild(box);
+  }
+  canvas.appendChild(columnDiv);
 }
 
-const clearBtn = document.querySelector('.clear');
+const clearBtn = document.querySelector(".clear");
 
-clearBtn.addEventListener('click', () => {
-    const allSquares = canvas.querySelectorAll('div');
-    allSquares.forEach(square => {
-        square.style.backgroundColor = 'white';
-    })
-})
+clearBtn.addEventListener("click", () => {
+  const allSquares = canvas.querySelectorAll("div");
+  allSquares.forEach((square) => {
+    square.style.backgroundColor = "white";
+  });
+});
